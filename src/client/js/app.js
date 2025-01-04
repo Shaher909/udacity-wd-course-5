1dac4ae986915eb2048282a12a155be40b2c21d0
shaher.je <shaher.jamaleldin@gmail.com>
2025-01-04 09:12:32 +0100

    feat: Validate date input

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,3 +2,20 @@ export function test() {
   console.log("test");
   alert("test");
 }
+
+// Client side validation to having a valid date
+function dateInputValidation (){
+  const dateInput = document.getElementById('departure-date');
+
+  dateInput.addEventListener('change', () => {
+  const selectedDate = new Date(dateInput.value);
+  const today = new Date();
+
+  if (selectedDate < today) {
+    alert("Please select a future date."); 
+    dateInput.value = ''; 
+  }
+});
+}
+
+dateInputValidation();
